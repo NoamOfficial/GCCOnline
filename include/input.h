781c56531607ba53daac535a5a3072ba3e6b2862
NoamOfficial <noamrosenblit@gmail.com>
2025-12-05 15:06:32 +0200

    Update input.h

--- include/input.h
+++ include/input.h
@@ -1,8 +1,10 @@
-void GetInput(void)
-int input
-_asm("mov al, 0xF4")
-_asm("out 0x64, al")
-_asm("in al, 0x60")
-_asm("mov %input, al")
-  return input
-  
+static inline unsigned char GetInput(void) {
+    unsigned char input;
+    asm volatile (
+        "inb $0x60, %0"   // read from keyboard data port
+        : "=a" (input)    // output in AL -> input
+        :                 // no inputs
+        :                 // no clobbers
+    );
+    return input;
+}
