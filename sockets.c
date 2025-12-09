ba5568316a57c0cfeeeacba9f8c5a5520e6a23cc
NoamOfficial <noamrosenblit@gmail.com>
2025-12-09 20:06:34 +0200

    Update sockets.c

--- sockets.c
+++ sockets.c
@@ -1,12 +1,23 @@
+// First, define basic integer types if not included
+typedef unsigned char  u8;
+typedef unsigned short u16;
+typedef unsigned int   u32;
+
+// ------------------- SOCKET STRUCT -------------------
 typedef struct {
-u32 DestAddress
-u32 Data
-} Socket
- void RecvData()
-return Data;
-void SendData(u32 newdata)
-int i = 1;
-for i=0; 1; [data]=newdata;
-while(i=0);
-return Data;
+    u32 DestAddress;
+    u16 DestPort;
+    u32 Data;
+} Socket;
+
+// ------------------- SOCKET FUNCTIONS -------------------
+u32 GetData(Socket* s) {
+    return s->Data;
+}
+
+void SendData(Socket* s, u32 newdata) {
+    // For demonstration, just store the data in the socket
+    s->Data = newdata;
+}
+
 
