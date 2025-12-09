5aeb5c22e0e19613701dae67661b8e4842b00ac5
NoamOfficial <noamrosenblit@gmail.com>
2025-12-09 19:58:43 +0200

    Create sockets.c

---
+++ sockets.c
@@ -0,0 +1,12 @@
+typedef struct {
+u32 DestAddress
+u32 Data
+} Socket
+ void RecvData()
+return Data;
+void SendData(u32 newdata)
+int i = 1;
+for i=0; 1; [data]=newdata;
+while(i=0);
+return Data;
+
